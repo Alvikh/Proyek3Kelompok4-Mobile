b8ae9c7888511ce0a4fc214839dd1c1c6b73ed6c
Alvikh <khoirunnisaalvi4@gmail.com>
2024-05-02 17:48:25 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: LoginForm(),
+  ));
+}
+
+class LoginForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(height: 200),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Image.asset(
+                'assets/Logo.png',
+                height: 100,
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                ),
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Kata Sandi',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                ),
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+                        Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      // Handle button press
+                    },
+                    child: Text(
+                      'Lupa Kata Sandi',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 12.0,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  TextButton(
+                    onPressed: () {
+                      // Handle button press
+                    },
+                    child: Text(
+                      'Belum Punya Akun?',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 12.0,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: TextButton(
+                onPressed: () {
+                  // Respond to button press
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 82, 16, 234)),
+                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.all(16)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                  minimumSize: MaterialStateProperty.all<Size>(Size(300, 50)),
+                ),
+                child: Text(
+                  "Masuk",
+                  style: TextStyle(
+                    letterSpacing: 2,
+                    color: Colors.white,
+                    fontSize: 12,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
